8ce3692302b4fa41038068e3fbe48a9a396a17af
joey levin <levinjoey@joeys-mbp.lan>
2019-02-28 10:19:42 -0800

    added get user

--- src/actions/databaseActions.js
+++ src/actions/databaseActions.js
@@ -2,19 +2,31 @@ import { USER_INFO_UPDATE } from './types';
 import * as firebase from 'firebase';
 // firebase.initializeApp(firebaseConfig);
 
-export const updateUserInfo = (category, answer) => {
+export const updateUserInfo = (category, answer) => next => {
 	let user = firebase.auth().currentUser;
 	let userID = user.uid;
 	let userFirebase = firebase.database().ref('/users/'+userID);
     userFirebase.child(category).set(answer);
+    console.log("testing", getUserInfo("denomination"));
 };
 
-export const getUserInfo = (category) => {
+export const getUserInfo = (category) => next => {
 	let user = firebase.auth().currentUser;
 	let userID = user.uid;
 	var ret = "";
 	let userFirebase = firebase.database().ref('/users/'+userID);
     userFirebase.child(category).on("value", function(snapshot) {
+    	ret = snapshot.val();
+    })
+    return ret;
+}
+
+export const getUser = () => {
+	let user = firebase.auth().currentUser;
+	let userID = user.uid;
+	var ret = "";
+	let userFirebase = firebase.database().ref('/users/'+userID);
+    userFirebase.on("value", function(snapshot) {
     	ret = snapshot.val();
     })
     return ret;
