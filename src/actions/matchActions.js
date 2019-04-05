c0488dbe720492900d2edb5a0f1708e1704f0603
joey levin <levinjoey@joeys-mbp.lan>
2019-04-04 17:03:36 -0700

    Updated get Candidate and match categories

--- src/actions/matchActions.js
+++ src/actions/matchActions.js
@@ -1,5 +1,5 @@
 import * as firebase from 'firebase';
-import { USER_MATCH_UPDATE_SUCCESS, GET_MATCHES_SUCCESS } from './types';
+import { USER_MATCH_UPDATE_SUCCESS, GET_MATCHES_SUCCESS, POSITIVE_MATCH, NEGATIVE_MATCH, MUTUAL_MATCH } from './types';
 
 export const userMatchUpdateSuccess = (category, matchId) => ({
     type: USER_MATCH_UPDATE_SUCCESS,
@@ -40,6 +40,7 @@ export const getMatches = (category) => dispatch =>  {
 
 //Creates/updates the match category that the matching user belongs to
 export const addMatch = (category, matchID) => dispatch =>{
+
     let user = firebase.auth().currentUser;
     let userID = user.uid;
     let userCategoryRef = firebase.database().ref('/users/'+userID+"/matches/"+matchID);
@@ -52,17 +53,31 @@ export const addMatch = (category, matchID) => dispatch =>{
 export const getCandidate = () => dispatch => {
     let user = firebase.auth().currentUser;
     let userID = user.uid;
-    firebase.database().ref('/users/'+userID+'/info').on('value', function(snapshot) {
-        let userGender =  snapshot.gender;
+    //Get database reference to current user
+    firebase.database().ref('/users/'+userID).on('value', function(snapshot) {
+        console.log(snapshot);
+        let userGender =  snapshot.val().info.gender;
+        let userMatches = new Array();
+        Object.keys(snapshot.val().matches).forEach(value=>{
+            userMatches.push(value.key);
+        });
         let userCategoryRef = firebase.database().ref('/users/');
-        let priorMatches = new Array();
-        priorMatches.push(addMatch('matches'));
+        //Iterate over all users in the database
         userCategoryRef.on("value",
-            function(snapshot) {
-                snapshot.forEach(value=>{
-                    if (!priorMatches.includes(value.key)) {
-                        if (userGender != value.info.gender) {
-                            return value.key;
+            function(snapshot2) {
+                snapshot2.forEach(potentialMatch=>{
+                    //Check that didn't match previously with them
+                    if (!userMatches.includes(potentialMatch.key)) {
+                        if (userGender != potentialMatch.val().info.gender) {
+                            //check if potential match matched with you
+                            if (potentialMatch.val().matches.userID != undefined) {
+                                if (potentialMatch.val().matches.userID.group != 'never') {
+                                    return potentialMatch.key;
+                                }
+                            }
+                            else {
+                                return potentialMatch.key;
+                            }
                         }
                     }
                 });
