cf339d62d2372a4917a3a3f762488ad1b7133d35
Asher Naghi <ashernaghi@gmail.com>
2019-04-11 13:53:09 -0700

    contacts problem

--- src/screens/ContactsModal.js
+++ src/screens/ContactsModal.js
@@ -12,28 +12,33 @@ export class ContactsModal extends React.Component {
 
 //Workingish Code Starts Here
 
-//   _retrieveData = async () => {
-//   try {
-//     const value = await Contacts.getContactsAsync({
-//       fields:  [Contacts.Fields.PhoneNumbers, Contacts.Fields.Emails, Contacts.Fields.SocialProfiles, Contacts.Fields.Relationships,],
-//     });
-//
-//     if (value !== null) {
-//       //console.log(value)
-//         let contactnamearray = []
-//         value.data.forEach(userValue=>{
-//           contactnamearray.push(<ProfileCard title={userValue.name} />)
-//
-//           return contactnamearrays;
-//         });
-//     } else {
-//       return(<ActivityIndicator/>)
-//     }
-//
-//   } catch (error) {
-//     // Error retrieving data
-//   }
-// };
+  _retrieveData = async () => {
+  try {
+    const value = await Contacts.getContactsAsync({
+      fields:  [Contacts.Fields.PhoneNumbers, Contacts.Fields.Emails, Contacts.Fields.SocialProfiles, Contacts.Fields.Relationships,],
+    });
+
+    if (value !== null) {
+      //console.log(value.data.name[0])
+        let contactNameArray = []
+        value.data.forEach(userValue=>{
+          contactNameArray.push(userValue.name)
+        })
+        console.log(contactnamearray);
+        let betterContactNameArray = []
+        betterContactNameArray = contactnamearray.forEach(nameOfContact=>{<View><Text>{nameOfContact}</Text></View>
+        }
+        )
+        console.log('-------------')
+        console.log(betterContactNameArray);
+    } else {
+      return(<ActivityIndicator/>)
+    }
+
+  } catch (error) {
+    // Error retrieving data
+  }
+};
 
 //Workingish Code Ends here
 
@@ -54,75 +59,36 @@ export class ContactsModal extends React.Component {
 
 
   render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>This feature is under construction...</Text>
-      <Button
-                   onPress={() => this.props.navigation.goBack()}
-                   title="Done"
-                   />
-      </View>
-  )
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+  //     {}
+  //     <Text>This feature is under construction...</Text>
+  //     <Button
+  //                  onPress={() => this.props.navigation.goBack()}
+  //                  title="Done"
+  //                  />
+  //     </View>
+  // )
 
-    // return (
-    //   <SafeAreaView style={{ flex: 1, backgroundColor: '#F4F4F4' }}>
-    //         <View  style={{ flex: 1, backgroundColor: 'blue', flexDirection: 'row', justifyContent: 'flex-end' }}>
-    //           <Button
-    //               onPress={() => this.props.navigation.goBack()}
-    //               title="Done"
-    //               />
-    //           </View>
-    //         <View style={{ flex: 1, backgroundColor: 'red', flexDirection: 'row', justifyContent: 'center', alignSelf: 'center', }}>
-    //           <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Contacts</Text>
-    //         </View>
-    //         <View style={{ flex: 18, backgroundColor: 'pink' }}>
-    //         <ScrollView>
-    //         <Button title='Touch Me' onPress={this.showContacts}/>
-    //
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         <Text>HI</Text>
-    //         </ScrollView>
-    //         </View>
-    //
-    //   </SafeAreaView>
-    // );
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#F4F4F4' }}>
+            <View  style={{ flex: 1, backgroundColor: 'blue', flexDirection: 'row', justifyContent: 'flex-end' }}>
+              <Button
+                  onPress={() => this.props.navigation.goBack()}
+                  title="Done"
+                  />
+              </View>
+            <View style={{ flex: 1, backgroundColor: 'red', flexDirection: 'row', justifyContent: 'center', alignSelf: 'center', }}>
+              <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Contacts</Text>
+            </View>
+            <View style={{ flex: 18, backgroundColor: 'pink' }}>
+            <ScrollView>
+            <Button title='Touch Me' onPress={() => this._retrieveData()}/>
+            </ScrollView>
+            </View>
+
+      </SafeAreaView>
+    );
   }
 }
 
