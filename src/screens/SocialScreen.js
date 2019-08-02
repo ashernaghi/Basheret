e9b1924442671750efd7cf441d800dcad537552d
Asher Naghi <ashernaghi@gmail.com>
2019-08-02 16:00:30 -0700

    social page is now just the matches, started styling

--- src/screens/SocialScreen.js
+++ src/screens/SocialScreen.js
@@ -1,74 +1,176 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, ScrollView, SafeAreaView, Text, Image, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import SwitchSelector from "react-native-switch-selector";
 import ChatScreen from './ChatScreen';
 import MatchesScreen from './MatchesScreen';
-import styles from '../styles/styles';
+//import styles from '../styles/styles';
 import {showProfileScreen} from '../actions/UserInfoActions';
+import {getCurrentMatches} from '../actions/matchActions';
+
 
 export class SocialScreen extends React.Component {
-  constructor(props){
-    super(props)
-
-    this.state={
-      show: "matches",
-    }
-  }
-
   static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: (
-        <TouchableOpacity style={ styles.touchableOpacityHeader } onPress={() => navigation.navigate('Home')}>
-          <Ionicons
-            name="ios-arrow-back"
-            size={32}
-            color="black"
-            style={styles.headerIcons}
-          />
-          <Text style={ styles.headerText } >
-            Home
-          </Text>
-        </TouchableOpacity>
-      ),
+      header: null,
     }
   };
 
-  render() {
-    let renderComponent;
-    if(this.state.show==='matches'){
-      renderComponent = <MatchesScreen
-        navigate={(card)=> {
-          console.log('HERE IN NAV')
-          this.props.dispatch(showProfileScreen('match', card));
-          this.props.navigation.navigate('CandidateModal')}
-        }
-        navigation={this.props.navigation}
-      />
+  componentWillMount(){
+    // getCurrentMatches();
+    if (!this.props.matchesCards) {
+        this.props.dispatch(getCurrentMatches());
     }
-    else{
-      renderComponent = <ChatScreen/>
-    }
+  }
 
-    return (
-      <View style={{flex: 1, alignItems: 'center' }}>
-        <SwitchSelector
-          initial={0}
-          imageStyle={{justifyContent: 'center', alignItems: 'center'}}
-          onPress={value => this.setState({show: value})}
-          buttonColor='#7ca6ea'
-          height={50}
-          borderRadius='0'
-          options={[
-            { value: "matches", label: 'Matches' },
-            { value: "chats", label: 'Chats'  },
-          ]}
-        />
-        {renderComponent}
-      </View>
-    );
+  generateMatchCards(){
+    const { navigate } = this.props.navigation;
+    if(this.props.matchesCards) {
+      return this.props.matchesCards.map((matchCard, index)=>{
+        return (
+          <TouchableOpacity key={index} style={styles.matchCardStyle} onPress={() => navigate('Chat', {m: matchCard})}>
+
+            <TouchableOpacity style={styles.thumbnailContainerStyle} onPress={()=>this.props.navigate(matchCard)}>
+              <Image style={styles.imageStyle} source={{ uri: matchCard.profilePhoto }}/>
+            </TouchableOpacity>
+
+            <View style={styles.textStyle}>
+              <View
+              //show Profile Modal and change profile to 'candidate'
+              >
+              <Text key={index} style={styles.titleStyle} >{matchCard.name}</Text>
+              <Text style={styles.bodyStyle}>This is a test of the text df dfdf fdfdfdfd fdfdf fd fd  portion of the matchcard...</Text>
+              </View>
+            </View>
+            <View style={ styles.buttonStyle }>
+            <View>
+              <Ionicons
+                name="ios-arrow-forward"
+                size={30}
+                color="grey"
+                style={styles.headerIcons}
+              />
+            </View>
+            </View>
+          </TouchableOpacity>
+        )
+      })
+    } else {
+      return(
+        <View style={{ alignSelf: 'center', }}>
+        <Text style={{ fontSize: 20 }}>No Luck...Soon by you!</Text>
+        </View>
+      )
+    }
+  }
+
+  render() {
+    //console.log('MATCHCARDPROPS', this.props.matchesCards)
+
+      return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#f4f4f4' }}>
+          <View style={styles.headerContainerStyle}>
+            <View style={ styles.backArrowStyle }>
+              <Ionicons
+                name="ios-arrow-back"
+                size={27}
+                color="grey"
+                onPress={() => this.props.navigation.navigate('Home')}
+              />
+            </View>
+            <View style={styles.logoContainerStyle}>
+              <Text style={styles.logoFontStyle}>Matches</Text>
+            </View>
+          </View>
+        <ScrollView style={{ alignSelf: 'stretch' }}>
+          {this.generateMatchCards()}
+        </ScrollView>
+      </SafeAreaView>
+      )
+
   }
 }
 
-export default connect()(SocialScreen);
+const mapStateToProps = state => {
+  return {
+    matches: state.userInfo.user.matches,
+    matchesCards: state.userInfo.user.matchesCards
+  };
+};
+
+export default connect(mapStateToProps)(SocialScreen);
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    flex: 1
+  },
+
+  headerContainerStyle: {
+   flexDirection: 'row',
+   justifyContent: 'center',
+   borderBottomColor: 'grey',
+   borderBottomWidth: 0.75,
+   paddingBottom: 20,
+ },
+
+ backArrowStyle:{
+   alignSelf: 'center',
+   justifyContent:'flex-start',
+ },
+
+ logoContainerStyle:{
+   alignSelf: 'center',
+   alignItems:'center',
+   justifyContent: 'center',
+   marginRight: 90,
+   marginLeft: 90, },
+
+ logoFontStyle: {
+   fontSize: 50,
+   fontFamily: 'fitamint-script',
+   color: '#00387e',
+ },
+
+  matchCardStyle: {
+    flexDirection: 'row',
+    borderBottomWidth: 0.75,
+    borderColor: 'grey',
+    height: 85,
+    width: 345,
+    alignSelf: 'flex-end',
+  },
+
+  imageStyle: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'grey',
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textStyle: {
+    // alignSelf: 'center',
+    marginLeft: 15,
+    marginTop: 10,
+    maxWidth: 230,
+    maxHeight:30
+  },
+  titleStyle: {
+    fontSize: 16,
+    fontWeight: 700,
+    paddingBottom: 5,
+  },
+  bodyStyle:{
+    fontSize: 15,
+  },
+  buttonStyle: {
+    justifyContent: 'center',
+    marginLeft: 'auto',
+    width: 30
+  }
+})
