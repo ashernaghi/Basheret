7416dd9b527941a53aecabdb1d1df6b5218d0028
DanielTarko <dtarko2020@yula.org>
2019-10-17 08:04:57 -0700

    added a page to select contacts for match maker

--- src/screens/contactList.js
+++ src/screens/contactList.js
@@ -18,228 +18,91 @@ import firebase from "../../src/actions/firebase";
 import { SearchBar } from "react-native-elements";
 import SearchInput, { createFilter } from "react-native-search-filter";
 import HomeScreen from './HomeScreen';
+import { Ionicons } from '@expo/vector-icons';
 class ContactList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoading: false,
+  static navigationOptions = ({ navigation }) => {
+    return {
+      header: null,
+    }
+  };
+
+  constructor(props) {
+   super(props);
+    this.state={
       contacts: [],
-      phoneConatcts: [],
-      newArray: [],
-      search: "",
-      data12: []
-    };
+      isLoading: true,
+    } 
   }
 
-  loadContacts = () => {
-    var contact1 = {
-      name: "",
-      number: ""
-    };
-    var contact2 = [];
-    firebase
-      .firestore()
-      .collection("users")
-      .get()
-      .then(
-        function(querySnapshot) {
-          querySnapshot.forEach(function(doc) {
-            if (doc.data().info.phoneNumber != undefined) {
-              if (doc.data().info.name != undefined) {
-                contact2.push({
-                  name: doc.data().info.name,
-                  number: doc.data().info.phoneNumber,
-                  profilePhoto: doc.data().info.profilePhoto,
-                  userID: doc.id
-                });
-              } else {
-                contact2.push({
-                  name: "No Name",
-                  number: doc.data().info.phoneNumber,
-                  profilePhoto: doc.data().info.profilePhoto,
-                  userID: doc.id
-                });
-              }
-            }
-          });
+ loadContact = async()=> {
 
-          this.setState({
-            contacts: contact2,
-            isLoading: false
-          });
-          var arr1 = contact2;
-          var newArr = this.state.phoneConatcts;
+    const {data} = await Contacts.getContactsAsync({
+      fields:[Contacts.Fields.PhoneNumbers, Contacts.Fields.Image]
+    })
 
-          var myArray = [];
-          arr1.forEach(item1 => {
-            newArr.forEach(item2 => {
-              if (item1.number === item2.number) {
-                item2.userID = item1.userID;
-                item2.name = item1.name;
-                item2.profilePhoto = item1.profilePhoto;
-                myArray.unshift(item1);
-              } else {
-                myArray.unshift(item2);
-              }
-            });
-          });
-          this.setState({
-            newArray: newArr,
-            data12: myArray
-          });
-        }.bind(this)
-      );
-  };
-
-  async showFirstContactAsync() {
-    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
-      title: "Contacts",
-      message: "This app would like to view your contacts."
-    }).then(() => {
-      console.log("Permission successfull");
-    });
-    const contacts = await Contacts.getContactsAsync().then(contacts => {
-      var data = contacts.data;
-      var contactMobile = [];
-      data.forEach((doc, index) => {
-        if (doc.phoneNumbers != undefined) {
-          const a = doc.phoneNumbers;
-          const [b] = a;
-          var c = {
-            name: doc.name,
-            number: b.number,
-            profilePhoto: "",
-            userID: ""
-          };
-          contactMobile.push(c);
-        }
-      });
-      this.setState({
-        phoneConatcts: contactMobile
-      });
-    });
+    this.setState({contacts: data})
+    console.log(data);
   }
 
-  searchFilterFunction = text => {
-    const newData = this.state.newArray.filter(item => {
-      const itemData = item.name;
-      const textData = text.toUpperCase();
-
-      return itemData.indexOf(textData) > -1;
-    });
-    this.setState({ data12: newData });
-  };
-
-  searchFunction = () => {
-    const filtered = this.state.newArray.filter(
-      createFilter(this.state.search, ["name"])
-    );
-    this.setState({ data12: filtered });
-  };
-
-  componentDidMount() {
-    this.showFirstContactAsync();
-    // console.log("In this state user is :::....");
-    // console.log(this.props.navigation.state.params.user);
-    this.setState({ isLoading: true });
-    this.loadContacts();
+  async componentDidMount(){
+    this.loadContact()
   }
-  // console.log("This is good touch :: " + item.userID);
-  // console.log("This is profile photo :: " + item.profilePhoto);
-  renderItem = ({ item }) => (
-    <TouchableOpacity
-      onPress={() => {
-        if (item.userID != "") {
-          if (this.props.navigation.state.params.user === 1) {
-            let data = {
-              userId1: item.userID,
-              username1: item.name,
-              userProfile1: item.profilePhoto
-            };
-            this.props.adduser1(data);
-          } else {
-            let data = {
-              userId2: item.userID,
-              username2: item.name,
-              userProfile2: item.profilePhoto
-            };
-            this.props.adduser2(data);
-          }
-        } else {
-          const { result } = SMS.sendSMSAsync(
-            [item.number],
-            "Please click on following link to download App"
-          );
-        }
 
-this.props.navigation.goBack(null);
-      }}>
-      <View style={{ minHeight: 70, padding: 5 }}>
-        <Text
-          style={{
-            color: "rgb(255, 108, 117)",
-            fontWeight: "bold",
-            fontSize: 26
-          }}>
-          {item.name}
-        </Text>
-        <Text style={{ color: "rgb(255, 108, 117)", fontWeight: "bold" }}>
-          {item.number}
-        </Text>
-        <Text style={{ color: "rgb(255, 108, 117)" }}>
-          {item.userID !== "" ? "From app contacts" : "From phone contacts"}
-        </Text>
+  renderItem = ({item}) =>(
+    <TouchableOpacity>
+      <View style={styles.contactCard}>
+        <View style={{paddingLeft: 20, paddingRight: 20, alignSelf: 'center',}}>
+          {this.renderPhoto(item)}
+        </View>
+        <View style={{paddingTop: 23}}>
+          <Text style={styles.nameStyle}>
+            {item.firstName}{item.lastName}
+          </Text>
+          <Text style={styles.foundIn}>
+            found in contacts
+          </Text>
+        </View>
+      </View>
+      <View style={{height: 1, width: '100%', backgroundColor: 'grey', marginLeft: 20}}>
       </View>
     </TouchableOpacity>
-  );
-  updateSearch = search => {
-    this.setState({ search: search });
-  };
-  render() {
-    const search = this.state.search;
-    return (
-      <View style={{ flex: 1 }}>
-        <SafeAreaView style={{ backgroundColor: "#2f363c" }} />
+  )
 
-        <SearchBar
-          placeholder='Type Here...'
-          round
-          searchIcon={{ size: 24 }}
-          onChangeText={text => {
-            this.setState({ search: text });
-            this.searchFunction();
-          }}
-          value={search}
-        />
-        <View style={{ flex: 1, backgroundColor: "white" }}>
-          {this.state.isLoading ? (
-            <View
-              style={{
-                ...StyleSheet.absoluteFill,
-                alignItems: "center",
-                justifyContent: "center"
-              }}>
-              <ActivityIndicator size='large' color='rgb(255, 108, 117)' />
-            </View>
-          ) : null}
-          <FlatList
-            data={this.state.data12}
-            renderItem={this.renderItem}
-            keyExtractor={(item, index) => index.toString()}
-            ListEmptyComponent={() => (
-              <View
-                style={{
-                  flex: 1,
-                  alignItems: "center",
-                  justifyContent: "center",
-                  marginTop: 50
-                }}>
-                <Text style={{ color: "rgb(255, 108, 117)" }}>
-                  No Contacts Found
-                </Text>
-              </View>
-            )}
+  renderPhoto = ({item}) =>(
+      <Ionicons
+        name={'ios-contact'}
+        size={55}
+        color="grey"
+      />
+  )
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
+          <Ionicons
+            name={'ios-close'}
+            size={40}
+            color="grey"
+            style={styles.closeIcon}
           />
+        </TouchableOpacity>
+        <Text style={styles.headerStyle}>Contacts</Text>
+        <View style={{paddingTop: 20}}>
+          <SearchBar
+            placeholder="Search"
+            containerStyle={{backgroundColor: '#C4C4C4', height: 45, borderBottomColor: 'transparent', borderTopColor: 'transparent'}}
+            inputContainerStyle={{backgroundColor: '#EAE8E8', borderRadius: 15, height: 22}}
+          />
+          <View>
+            
+            <FlatList 
+             data={this.state.contacts}
+             renderItem={this.renderItem}
+             listEmptyComponent={()=><Text>No Contacts Found</Text>}
+             keyExtractor={(item, index) => index.toString()}
+            />
+          </View>
         </View>
       </View>
     );
@@ -252,24 +115,45 @@ function mapStateToProps(state) {
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    adduser1: data => dispatch({ type: "ADD_USER_1", payload: data }),
-    adduser2: data => dispatch({ type: "ADD_USER_2", payload: data })
-  };
-}
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ContactList);
+export default connect( mapStateToProps )(ContactList);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 20,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+  },
+
+  headerStyle: {
+    backgroundColor: "#fff",
+    fontWeight: 'bold', 
+    fontFamily: 'fitamint-script', 
+    fontSize: 50, 
+    paddingLeft: 16,
+    paddingTop: 15,
+  },
+
+  contactCard: {
+    height: 80,
+    flex: 1,
+    flexDirection: 'row',
+  },
+
+  closeIcon: {
+    paddingTop: 35,
+    alignSelf: 'flex-end',
+    paddingRight: 30,
+  },
+
+  nameStyle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'helvetica-neue'
+  },
+
+  foundIn: {
+    color: 'grey',
+    fontSize: 10,
+    fontWeight: 'bold',
+  },
 });
